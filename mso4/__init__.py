d9afcd65befc2d761c739c667ad6ee6d4babf5eb
HOANGNN30 <34944337+Hoangnn30@users.noreply.github.com>
2026-09-24 14:13:51 +0700

    feat: export TekHSI waveform client

--- mso4/__init__.py
+++ mso4/__init__.py
@@ -1,4 +1,11 @@
 from .scpi import MSO4Client, MSO4Error
+from .hsi import TekHSIUnavailable, TekHSIWaveformClient
 from .waveform import Waveform
 
-__all__ = ["MSO4Client", "MSO4Error", "Waveform"]
+__all__ = [
+    "MSO4Client",
+    "MSO4Error",
+    "TekHSIUnavailable",
+    "TekHSIWaveformClient",
+    "Waveform",
+]
